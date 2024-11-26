70945a5d9efddb25b729c997c6367152219bae5a
Elxan <elxan.offical.07@mail.com>
2024-11-26 23:45:30 +0400

    about section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Features from "./components/Features";
+import About from "./components/About";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Header />
       <Home />
       <Features/>
+      <About/>
     </>
   );
 };
---
+++ client/src/components/About.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import about from '../assets/about.svg'
+import about2 from '../assets/about2.svg'
+
+const About = () => {
+  return (
+    <div className='md:px-14 p-4 max-w-screen-2xl mx-auto space-y-10'>
+        <div className='flex flex-col md:flex-row justify-between items-center gap-8'>
+            <div className='md:w-1/2'>
+                <img src={about} alt="" />
+            </div>
+
+            {/* about content*/}
+            <div className='md:w-2/5'>
+                <h2 className=' md:text-6xl text-3xl text-primary mb-5 leading-normal'>
+                    Lorem ipsum dolor sit amet consectetur. <span className='text-secondary'>Lorem, ipsum dolor.</span>
+                </h2>
+                <p className='text-gray-700 text-lg mb-7'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore consequatur accusamus repellendus?
+                     Vitae blanditiis sunt sint illo molestiae suscipit ut?</p>
+                     <button className='btnPrimary'>Get Started</button>
+            </div>
+        </div>
+
+
+        <div className='flex flex-col md:flex-row-reverse justify-between items-center gap-8'>
+            <div className='md:w-1/2'>
+                <img src={about2} alt="" />
+            </div>
+
+            {/* about content*/}
+            <div className='md:w-2/5'>
+                <h2 className=' md:text-6xl text-3xl text-primary mb-5 leading-normal'>
+                    Lorem ipsum dolor sit amet consectetur. <span className='text-secondary'>Lorem, ipsum dolor.</span>
+                </h2>
+                <p className='text-gray-700 text-lg mb-7'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore consequatur accusamus repellendus?
+                     Vitae blanditiis sunt sint illo molestiae suscipit ut?</p>
+                     <button className='btnPrimary'>Get Started</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
